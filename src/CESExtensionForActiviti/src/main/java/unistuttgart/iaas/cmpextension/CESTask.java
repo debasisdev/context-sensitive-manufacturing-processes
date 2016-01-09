1390a9e03ffb8c5e73e7a2928144888d7b15e5ab
Debasis Kar <debasis.babun@gmail.com>
2016-01-09 06:49:06 +0100

    Activiti Extension for Integration Improved and Input Fields are added.

--- src/CESExtensionForActiviti/src/main/java/unistuttgart/iaas/cmpextension/CESTask.java
+++ src/CESExtensionForActiviti/src/main/java/unistuttgart/iaas/cmpextension/CESTask.java
@@ -19,12 +19,12 @@ import org.activiti.designer.integration.servicetask.PropertyType;
  * @since 27.12.2015
  */
 @Runtime(javaDelegateClass = "unistuttgart.iaas.cmpextension.runtime.CESTaskDelegation")
-@Help(displayHelpShort = "CES Task", 
-displayHelpLong = "Creates a Context-sensitive task that will deicde which process is best at a certain time. ")
+@Help(displayHelpShort = "Context-sensitive Execution Step", 
+displayHelpLong = "Creates a Context-sensitive Task that runs a process according to present scenario.")
 @TaskNames(
     {
-      @TaskName(locale = "en", name = "English CES Task"),
-      @TaskName(locale = "de", name = "Deutsche CES Aufgabe")
+      @TaskName(locale = "en", name = "CES Task"),
+      @TaskName(locale = "de", name = "CES Aufgabe")
     }
 )
 
@@ -34,31 +34,34 @@ public class CESTask extends AbstractCustomServiceTask {
   @Help(displayHelpShort = "Define the main goal/inention.", displayHelpLong = "You can provide the main intention of the task. It shouldn't contain any special character.")
   private String mainIntention;
      
-  @Property(type = PropertyType.TEXT, displayName = "Sub Intention(s)/Goal(s)", required = true, defaultValue = "highProfit, highLaborUtilization")
+  @Property(type = PropertyType.TEXT, displayName = "Sub Intention(s)/Goal(s)", required = true, defaultValue = "highAutomation, highThroughput")
   @Help(displayHelpShort = "Define the sub-goals/inentions.", displayHelpLong = "You can provide the sub intentions (if any) of the task separated by commas(,).")
   private String subIntentions;
   
-  @Property(type = PropertyType.TEXT, displayName = "Required Context Data", required = true, defaultValue = "machine2Status, infraredSensorStatus")
+  @Property(type = PropertyType.TEXT, displayName = "Required Context Data", required = true, defaultValue = "shockDetectorStatus, unitsOrdered, availableWorkers, infraredSensorStatus")
   @Help(displayHelpShort = "Mentioned required context data.", displayHelpLong = "You can provide the list of required context names to be looked for separted by commas(,).")
   private String requiredContext;
   
-  @Property(type = PropertyType.TEXT, displayName = "Process Repository Location", required = true, defaultValue = "F:\\GitLab\\src\\main\\resources\\AutomatedHR.bpmn")
+  @Property(type = PropertyType.TEXT, displayName = "Process Repository Location", required = true, defaultValue = "\\src\\main\\resources\\processrepos\\ProcessRepository.xml")
   @Help(displayHelpShort = "Give the path to the process models' store.", displayHelpLong = "You can provide the list of required context names to be looked for separted by commas(,).")
   private String processRepositoryPath;
-
-  @Property(type = PropertyType.TEXT, displayName = "Result Variable", required = false)
-  @Help(displayHelpShort = "Give a variable to store final result.", displayHelpLong = "Final output would be stored in this variable.")
-  private String resultVariable;
   
-  @Property(type = PropertyType.RADIO_CHOICE, displayName = "Require Optimization", required = false)
+  @Property(type = PropertyType.TEXT, displayName = "Input Variable(s)", required = false, defaultValue = "operatorName = Wolfgang, supervisorName = Frank")
+  @Help(displayHelpShort = "Give any extra input as key-value pair.", displayHelpLong = "You can give any extra-input as a key-value pair separated by commas, e.g., age=25, experience=2.")
+  private String inputVariable;
+
+  @Property(type = PropertyType.TEXT, displayName = "Output Variable(s)", required = false, defaultValue = "finalStatus, packOutput")
+  @Help(displayHelpShort = "Give variable names to store final result.", displayHelpLong = "Final output would be stored in these variables separated by commas.")
+  private String outputVariable;
+  
+  @Property(type = PropertyType.RADIO_CHOICE, displayName = "Require Optimization", required = true, defaultValue = "True")
   @Help(displayHelpShort = "Should BPMN engine do any optimization?", displayHelpLong = "Choose the optimization criterion depending on the requirement and need of process model optimization in rumtime.")
-  @PropertyItems({"Yes (Do It, If Strategies are Available!)", "1", "No (Don't Do It!)", "0"})
+  @PropertyItems({"Yes (Do It, If Strategies are Available!)", "True", "No (Don't Do It!)", "False"})
   private String performOptimization;
- 
   
-  @Property(type = PropertyType.TEXT, visible = false, defaultValue = "test")
+  @Property(type = PropertyType.TEXT, visible = false, defaultValue = "demo")
   private String hiddenField;
-
+ 
   @Override
   public String contributeToPaletteDrawer() {
     return "IAAS - University of Stuttgart";
